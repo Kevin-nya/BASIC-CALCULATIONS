74adb1c959967db01da5010a41a4286cc05c89a9
Kevin-nya <nyanzikevin2003@gmail.com>
2024-11-15 20:00:44 +0300

    added a total_cost.c file and a program that calculates the total cost

---
+++ total_cost.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    // this program calculates the total cost of items
+    int unit_price = 1500;
+
+    int quantity = 2000;
+
+    int total_cost = unit_price * quantity;
+    
+    printf("total_cost = UGX %d", total_cost);
+
+    return 0;
+
+
+}
